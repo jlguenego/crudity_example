9da2031a79b67bd066b9a6ce4547c0dfde057137
Jean-Louis GUENEGO <jlguenego@gmail.com>
2020-08-31 15:32:25 +0200

    working

---
+++ server.ts
@@ -0,0 +1,14 @@
+import express from "express";
+import path from "path";
+import { Crudity } from "crudity";
+import { Article } from "./dto/article.dto";
+
+const app = express();
+const filename = path.resolve(__dirname, "./data/test.json");
+const articleRouter = new Crudity<Article>({ filename, dtoClass: Article })
+ .router;
+
+app.use(express.json());
+app.use("/ws/articles", articleRouter);
+
+app.listen(3000, () => console.log("Server started on port 3000"));
